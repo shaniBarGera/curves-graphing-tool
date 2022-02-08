7b8ccdd452a01cae0ab6830b2fdc6d5e9a63e5a4
shaniBarGera <shani.bla@gmail.com>
2022-02-08 04:49:54 +0200

    added info to equations

--- js/drop.js
+++ js/drop.js
@@ -36,23 +36,3 @@ window.addEventListener("drop", function(event){
     createWindow(td, title);
 });
 
-function test(){
-    addCol();
-    addCol();
-    addRow();
-
-    var td00 = document.getElementById("t_0_0");
-    createWindow(td00, "Monomial Basis");
-    var td01 = document.getElementById("t_0_1");
-    createWindow(td01, "Lagrange");
-    var td02 = document.getElementById("t_0_2");
-    createWindow(td02, "Cubic Hermite Spline");
-    var td10 = document.getElementById("t_1_0");
-    createWindow(td10, "Cubic Spline");
-    var td11 = document.getElementById("t_1_1");
-    createWindow(td11, "Bezier");
-    var td12 = document.getElementById("t_1_2");
-    createWindow(td12, "B-Spline");
-}
-
-test();
